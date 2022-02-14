aa764053b8cb7e6d9ee1aa5b9085021ed00eadc2
sravan <41821183+iosravan@users.noreply.github.com>
2022-02-14 20:20:19 +0530

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,1 +1,23 @@
 import discord
+import numpy as np
+import tensorflow as tf
+
+class Meta:
+  """ 
+  To create a personalised bot for discord 
+  requirements:
+        collect words, used phrases to auto reply
+        
+  """
+  pass
+  
+print(help(Meta))
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+  pass
